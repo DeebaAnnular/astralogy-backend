45a302ff3e41fd07a7c0f541430b76fb592bf687
DeebaAnnular <deebalakshmi.m@annulartechnologies>
2024-04-26 10:47:27 +0530

    http create server function added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParse = require("body-parser");
 const routes = require("./controller/users");
 const cors = require('cors');
+const http = require('http');
 
 
 require('dotenv').config();
@@ -19,3 +20,13 @@ app.use('/astra',routes);
 app.listen(port,()=>{
     console.log(`Listening Port: ${port}`)
 })
+
+// Create a separate HTTP server listening on port 80
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Connected to host\n');
+});
+
+server.listen(80, () => {
+    console.log('HTTP server listening on port 80');
+});
